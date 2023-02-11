1e9cf55d05d96ef4aee382549d087828e10e5772
Novikova Ksusha <sushi.91@mail.ru>
2023-02-11 13:13:25 +0700

    Добавила решение 64 задачи

---
+++ Задача64/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int count = 1;
+        NaturalToLow(number, count);
+
+
+        void NaturalToLow(int n, int count)
+        {
+            if (count > n)
+            {
+                return;
+            }
+            else
+            {
+                NaturalToLow(n, count + 1);
+                Console.Write(count + " ");
+            }
+        }
+    }
+}
